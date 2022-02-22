154cc6c1870352a5a24161d16fede922b047bdcc
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-21 17:59:17 -0700

    bar graph kinda working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import DataGrid from 'react-data-grid';
 import data from './data';
 import { createColumns, barData } from './data-utils';
-import { VictoryBar } from 'victory';
+import { VictoryAxis, VictoryBar, VictoryChart, VictoryTheme, VictoryPie } from 'victory';
+import { colors } from '@material-ui/core';
 
 
 function App() {
@@ -13,8 +14,46 @@ function App() {
       <DataGrid
         columns={createColumns(data)}
         rows={data} />
+      <VictoryChart 
+        theme={VictoryTheme.material}
+        domainPadding={{ x: 15, y: 15 }}
+      >
       
+        <VictoryAxis 
+         
+          style={{ 
+            axis: { stroke: ' #E0F2F1' },
+            axisLabel: { fontSize: 20 },
+            tickLabels: { fontSize: 5, padding: 10, angle: 30, fill: 'gray', fontWeight: 'bold' }
+          }} />
+        <VictoryAxis
+          dependentAxis
+          tickValues={[10, 20, 30, 40, 50, 60, 70]}
+          tickFormat={['10yrs', '20yrs', '30yrs', '40yrs', '50yrs', '60yrs', '70yrs']}
+
+          
+          
+        />
+
+        <VictoryBar data={barData(data)} 
+          x='color' y='averageAge' 
+          style={{ data: { fill: '#c43a31' } }}
+          alignment="start"
+          barRatio={.75}  
+         
+          
+        />
+      </VictoryChart>
+
+      <VictoryPie
+        data={[
+          { x: 'Cats', y: 35 },
+          { x: 'Dogs', y: 40 },
+          { x: 'Birds', y: 55 }
+        ]}
+      />
     </div>
+    
 
     
     
--- src/data-utils.js
+++ src/data-utils.js
@@ -11,5 +11,21 @@ export function createColumns(arr) {
 }
 
 export function barData(arr) {
-  
+  const count = arr.reduce((acc, curr) => {
+    if (acc[curr.favorite_color]) {
+      acc[curr.favorite_color].count++;
+      acc[curr.favorite_color].total = acc[curr.favorite_color].total + curr.age;
+    }
+    else {
+      acc[curr.favorite_color] = {};
+      acc[curr.favorite_color].count = 1;
+      acc[curr.favorite_color].total = curr.age;
+    }
+    return acc;
+  }, {});
+  return Object.entries(count)
+    .map(entry => ({
+      color: entry[0],
+      averageAge: entry[1].total / entry[1].count
+    }));
 }
