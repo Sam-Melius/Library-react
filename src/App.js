0be53555fc5ef558f8da17953eb1aed6424a7e91
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-21 18:54:36 -0700

    initial line graph and pie chart rendering, need to munge data

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import './App.css';
 import DataGrid from 'react-data-grid';
 import data from './data';
 import { createColumns, barData } from './data-utils';
-import { VictoryAxis, VictoryBar, VictoryChart, VictoryTheme, VictoryPie } from 'victory';
-import { colors } from '@material-ui/core';
-import { purple } from '@material-ui/core/colors';
+import { VictoryAxis, VictoryBar, VictoryChart, VictoryTheme, VictoryPie, VictoryLine } from 'victory';
+
 
 
 function App() {
@@ -65,6 +64,43 @@ function App() {
           
         
       />
+      </section>
+      <section>
+        <VictoryChart 
+        theme={VictoryTheme.material}
+        domainPadding={{ x: 15, y: 15 }}
+      >
+      
+        <VictoryAxis 
+         
+          style={{ 
+            axis: { stroke: ' #E0F2F1' },
+            axisLabel: { fontSize: 20 },
+            tickLabels: { fontSize: 5, padding: 10, angle: 30, fill: 'indigo', fontWeight: 'bold' }
+          }} />
+        <VictoryAxis
+          dependentAxis
+          tickValues={[10, 20, 30, 40, 50, 60, 70]}
+          tickFormat={['10yrs', '20yrs', '30yrs', '40yrs', '50yrs', '60yrs', '70yrs']}
+
+          
+          
+        />
+
+        <VictoryLine data={barData(data)} 
+          x='color' y='averageAge' 
+          
+         
+          animate={{
+            duration: 2000,
+            onLoad: { duration: 1000 }
+          }} 
+          interpolation="natural"
+         
+          
+        />
+        
+      </VictoryChart>
       </section>
 
 
